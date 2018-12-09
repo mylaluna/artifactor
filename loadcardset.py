55d769469a2bea624054f4ebfd710a02bed5eaaa
Rain S. Zhang <mylaluna@gmail.com>
2018-12-09 23:34:17 +1100

    Download artifact card image

--- loadcardset.py
+++ loadcardset.py
@@ -1,5 +1,7 @@
 import urllib.request
 import json
+import os
+import requests
 from datetime import datetime
 
 cardsetUrlJSONTemplate = {"cdn_root":"","url":"","expire_time":0}
@@ -8,13 +10,15 @@ cardsetUrlTemplate = "somehostsomeurl"
 setids = '00', '01'
 
 for setid in setids:
+  # pull cardsetUrlJSON from local file or Artifact API
   with open('cardsetUrl-' + setid + '.json', 'w+') as json_file:
     try:
       cardsetUrlJSON = json.load(json_file)
     except ValueError:
       cardsetUrlJSON = json.loads(json.dumps(cardsetUrlJSONTemplate))
       json.dump(cardsetUrlJSON, json_file, sort_keys=True, indent=2)
-
+  
+  # if cardsetUrlJSON has expired, request a new API and save it to file
   if datetime.now() >= datetime.fromtimestamp(cardsetUrlJSON['expire_time']):
     with urllib.request.urlopen(cardsetFetchUrlTemplate.replace("<setid>", setid)) as res: 
       cardsetUrlJSON = json.loads(res.read())
@@ -26,8 +30,33 @@ for setid in setids:
 
   print('importing card set ' + setid + ' from ' + cardsetUrl)
 
+  # pull cardsetJSON
   with urllib.request.urlopen(cardsetUrl) as res:
     cardsetJSON = json.loads(res.read())
-
+  
+  # save cardsetJSON to file
   with open('cardset-'+ setid+ '.json', 'w+') as outfile:
-    json.dump(cardsetJSON, outfile, sort_keys=True, indent=2) 
+    json.dump(cardsetJSON, outfile, sort_keys=True, indent=2)
+
+  # pull card images from cardsetJSON 
+  # if not os.path.exists('cardset_image'):
+  #   os.makedirs('cardset_image/' + setid + '/default/large_image')
+  #   os.makedirs('cardset_image/' + setid + '/default/mini_image')
+  #   os.makedirs('cardset_image/' + setid + '/schinese/large_image')
+  #   os.makedirs('cardset_image/' + setid + '/schinese/mini_image')
+  
+  for card in cardsetJSON['card_set']['card_list']:
+    # for language, name in card['card_name'].items():
+    
+    # ingame_image
+
+    # large_image
+    for language, image_url in card['large_image'].items():
+      with open('cardset_image/' + setid + '/' + language + '/large_image/' + card['card_name'][language] + '.png', 'wb') as imagefile:
+        imagefile.write(requests.get(image_url).content)
+    # mini_image
+
+
+  
+
+   
